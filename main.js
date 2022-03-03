e1ac552672809634ffb15ee4bf970460e549ce1d
chenmichael2 <chenmichael2012@gmail.com>
2022-03-03 00:03:50 -0800

    hopefully js works

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ console.log("script is working in main.js");
 
 const orderedList = document.querySelector("ol");
 const createListItem = document.createElement("li");
+const node = document.createTextNode("this is a new li");
+createListItem.appendChild(node);
