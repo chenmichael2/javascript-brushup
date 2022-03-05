61dd5b20043fc4a220c5edfb42183ca3bba9dd2c
chenmichael2 <chenmichael2012@gmail.com>
2022-03-04 23:14:57 -0800

    commiting hello world

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ console.log("script is working in main.js");
 const orderedList = document.querySelector("ol");
 
 const div = document.createElement("div");
-
+div.innerText = "Hello World";
 document.body.append(div);
