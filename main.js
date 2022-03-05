99c71ff0575baa548bcfb0d4738b2e2767398636
chenmichael2 <chenmichael2012@gmail.com>
2022-03-04 23:21:50 -0800

    adding all

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("script is working in main.js");
 
-const orderedList = document.querySelector("ol");
+const orderedList = document.querySelectorAll("ol");
 orderedList.createElement("li");
 
 // const div = document.createElement("div");
