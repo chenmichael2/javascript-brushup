bebba3c6654e06b948333aa3d9f9bf00cce5c51d
chenmichael2 <chenmichael2012@gmail.com>
2022-03-04 23:09:31 -0800

    console.log oL

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 console.log("script is working in main.js");
 
 const orderedList = document.querySelector("ol");
-const oL = orderedList.getElementsByClassName("ordered-list")
+const oL = orderedList.getElementsByClassName("ordered-list");
+console.log(oL);
