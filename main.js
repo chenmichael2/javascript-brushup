35fff6d6c566965d96a02dcfbc84ad5bc5b4f6e4
chenmichael2 <chenmichael2012@gmail.com>
2022-03-04 23:27:31 -0800

    adding create element

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ console.log("script is working in main.js");
 
 const orderedList = document.querySelector(".ordered-list");
 console.log(orderedList);
+orderedList.createElement("li");
 
 // const div = document.createElement("div");
 // div.innerText = "Hello World";
