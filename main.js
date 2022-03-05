810fd81dfd8fa4dd5d30a2ad9c0542fa8990416a
chenmichael2 <chenmichael2012@gmail.com>
2022-03-04 23:26:52 -0800

    trying to console.log

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("script is working in main.js");
 
 const orderedList = document.querySelector(".ordered-list");
-
+console.log(orderedList);
 
 // const div = document.createElement("div");
 // div.innerText = "Hello World";
