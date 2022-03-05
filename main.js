2b6ed24ea6554f0dc7b579e59189ef629db988d4
chenmichael2 <chenmichael2012@gmail.com>
2022-03-04 23:20:12 -0800

    adding an element li

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log("script is working in main.js");
 
 const orderedList = document.querySelector("ol");
+orderedList.createElement("li");
 
 // const div = document.createElement("div");
 // div.innerText = "Hello World";
