7c3a0d60bc5782eb4885ffc189c35117a6705358
chenmichael2 <chenmichael2012@gmail.com>
2022-03-04 23:04:14 -0800

    getting rid of things that don' t work

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
 console.log("script is working in main.js");
 
-const orderedList = document.querySelector("ol");
-const createListItem = document.createElement("li");
-const node = document.createTextNode("this is a new li");
-createListItem.appendChild(node);
-orderedList.appendChild(createListItem);
