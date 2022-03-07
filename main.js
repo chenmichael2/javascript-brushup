68ff0a8e013adb97b39a2d62d1cba5dd949bdb86
chenmichael2 <chenmichael2012@gmail.com>
2022-03-06 22:35:15 -0800

    commiting text content

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let orderedList = document.querySelector('ol');
 console.log(orderedList);
 let createListItem = document.createElement("li");
 let containText = createListItem.textContent("hello");
-orderedList.appendChild(createListItem);
+orderedList.appendChild(createListItem).textContent = "Hello"
 
 
 // const div = document.createElement("div");
