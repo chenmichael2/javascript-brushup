b8a5dac737f61543b8dab7f2910916a558ee306f
chenmichael2 <chenmichael2012@gmail.com>
2022-03-06 22:29:49 -0800

    containing text

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ console.log("script is working in main.js");
 let orderedList = document.querySelector('ol');
 console.log(orderedList);
 let createListItem = document.createElement("li");
+let containText = 
 orderedList.appendChild(createListItem);
+
 
 // const div = document.createElement("div");
 // div.innerText = "Hello World";
