f755467b0c9b11d0a8e6fcabf5e0fade990fa41d
chenmichael2 <chenmichael2012@gmail.com>
2022-03-06 22:50:51 -0800

    my button is working but its not adding elements

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 console.log("script is working in main.js");
 let clickButton = document.querySelector("button");
-
 let orderedList = document.querySelector('ol');
 let createListItem = document.createElement("li");
-// let containText = createListItem.textContent = "hello";
+let containText = createListItem.textContent = "hello";
+
 orderedList.appendChild(createListItem);
-orderedList.appendChild(createListItem);
+
 
 clickButton.onclick = () => {
     console.log("hello");
+    orderedList.appendChild(createListItem);
 }
 
 // const div = document.createElement("div");
