d15f15d9e76aec1993405549bac6e18e6027c391
chenmichael2 <chenmichael2012@gmail.com>
2022-03-06 22:32:56 -0800

    seeing if contain content works

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ console.log("script is working in main.js");
 let orderedList = document.querySelector('ol');
 console.log(orderedList);
 let createListItem = document.createElement("li");
-let containText = 
-orderedList.appendChild(createListItem);
+let containText = createListItem.textContent("hello");
+orderedList.appendChild(containText);
 
 
 // const div = document.createElement("div");
