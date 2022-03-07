1d05948148bf7b169a776b5aecb01d450d0065d1
chenmichael2 <chenmichael2012@gmail.com>
2022-03-06 22:12:12 -0800

    adding the variable again

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ console.log("script is working in main.js");
 let orderedList = document.querySelector('ol');
 console.log(orderedList);
 let createListItem = orderedList.createElement("li");
+createListItem
 
 // const div = document.createElement("div");
 // div.innerText = "Hello World";
