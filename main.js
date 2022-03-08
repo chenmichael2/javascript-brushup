59310f7057010e40cf0efffc836d4a7d51576c2f
chenmichael2 <chenmichael2012@gmail.com>
2022-03-07 21:25:36 -0800

    commenting out textcontent

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ console.log("script is working in main.js");
 let clickButton = document.querySelector("button");
 let orderedList = document.getElementsByClassName('ordered-list');
 let createListItem = document.createElement("li");
-
+// let containText = createListItem.textContent = "hello";
 
 console.log(orderedList);
 
 clickButton.onclick = () => {
     console.log("hello");
-
+    
 }
