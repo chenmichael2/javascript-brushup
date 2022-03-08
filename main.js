1bec8fac1f11ea9812e7dd582930b3b599c3406f
chenmichael2 <chenmichael2012@gmail.com>
2022-03-07 23:04:08 -0800

    adding a comment saying value is undefined

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let createListItem = document.createElement("li");
 let inputField = document.getElementsByClassName('input-field');
 
 console.log(orderedList);
-console.log(inputField.value); // this
+console.log(inputField.value); // this is undefined
 
 clickButton.addEventListener("click", (e) => {
     e.preventDefault();
