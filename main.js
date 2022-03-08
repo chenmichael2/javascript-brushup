a4a837fd11f7474a6639f94aa452a49615e65fc9
chenmichael2 <chenmichael2012@gmail.com>
2022-03-07 22:07:34 -0800

    seeing if the input field is selected

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let clickButton = document.querySelector("button");
 let orderedList = document.getElementsByClassName('ordered-list');
 let createListItem = document.createElement("li");
 // let containText = createListItem.textContent = "hello";
-let inputField = document
+let inputField = document.getElementsByClassName('input-field')
 
 console.log(orderedList);
 
