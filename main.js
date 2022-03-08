e2a237921fffc596c6be61ef8df3e31cc8aae5c3
chenmichael2 <chenmichael2012@gmail.com>
2022-03-07 20:50:40 -0800

    adding and manipulation

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 console.log("script is working in main.js");
 let clickButton = document.querySelector("button");
-let orderedList = document.querySelector('ol');
+let orderedList = document.getElementsByClassName('ordered-list');
 let createListItem = document.createElement("li");
 let containText = createListItem.textContent = "hello";
 
 orderedList.appendChild(createListItem);
 
-
+console.log(orderedList);
 clickButton.onclick = () => {
     console.log("hello");
-    console.log(orderedList);
+    orderedList.appendChild(createListItem);
 }
