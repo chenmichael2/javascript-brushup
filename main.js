b9911ec9eb744fefa679d2993f896ec21bab9d9d
chenmichael2 <chenmichael2012@gmail.com>
2022-03-07 21:24:41 -0800

    adding input field

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("script is working in main.js");
 let clickButton = document.querySelector("button");
 let orderedList = document.getElementsByClassName('ordered-list');
 let createListItem = document.createElement("li");
-let containText = createListItem.textContent = "hello";
+
 
 console.log(orderedList);
 
