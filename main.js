b924977f561b4991283be522f95c130077dfbfcd
chenmichael2 <chenmichael2012@gmail.com>
2022-03-09 23:04:46 -0800

    adding the document get element by id

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("script is working in main.js");
 let clickButton = document.querySelector("button");
 // let orderedList = document.getElementsByClassName('ordered-list');
-let orderedList = document.
+let orderedList = document.getElementById()
 let createListItem = document.createElement("li");
 // let containText = createListItem.textContent = "hello";
 let inputField = document.getElementById('input-field');
