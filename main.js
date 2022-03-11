af352d6bf41a791883e58994e4465cd8fd04ab38
chenmichael2 <chenmichael2012@gmail.com>
2022-03-10 21:15:06 -0800

    cleaning up more

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 console.log("script is working in main.js");
 let clickButton = document.querySelector("button");
 let orderedList = document.getElementById('ordered-list');
-
 let inputField = document.getElementById('input-field');
 
 // console.log(orderedList);
